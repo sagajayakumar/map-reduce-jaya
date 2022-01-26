1be7541d44e2cde0af3aec6ddbd9005fb89017bc
Jaya Kumar <jayakumar1994@gmail.com>
2022-01-26 13:43:47 -0600

    initial mapper file

---
+++ 01mapper.py
@@ -0,0 +1,14 @@
+# Jaya Kumar Saga
+# this is a example map sample program
+
+f = open("purchases.txt","r")           # open file, read-only
+o = open("01mapperOutput.txt","w")       # open file, write-only
+for line in f:  
+    dataList = line.strip().split("    ")    # count the spaces!
+    print (dataList   )
+    print (len(dataList  ))
+    if len(dataList  ) == 6:
+        date, time, location, dept, amount, payType = dataList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+f.close()
+o.close()
