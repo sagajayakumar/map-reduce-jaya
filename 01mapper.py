ddb928dfbf4b235c119ded24f11f33af2fefec2e
Jaya Kumar <jayakumar1994@gmail.com>
2022-01-26 13:50:58 -0600

    fixed data append issue to output file

--- 01mapper.py
+++ 01mapper.py
@@ -1,14 +1,15 @@
 # Jaya Kumar Saga
 # this is a example map sample program
 
-f = open("purchases.txt","r")           # open file, read-only
-o = open("01mapperOutput.txt","w")       # open file, write-only
+f = open("purchases.txt","r")  # open file, read-only
+o = open("01mapperOutput.txt", "w") # open file, write
 for line in f:  
-    dataList = line.strip().split("    ")    # count the spaces!
-    print (dataList   )
-    print (len(dataList  ))
-    if len(dataList  ) == 6:
+    dataList = line.strip().split("    ") 
+    print (dataList )
+    print (len(dataList ))
+    if len(dataList) == 6:
         date, time, location, dept, amount, payType = dataList  #assign names
         print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
 f.close()
 o.close()
